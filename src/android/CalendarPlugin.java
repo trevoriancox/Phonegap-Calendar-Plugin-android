ddcc612ead55c4abd65cd473cc0aa07911634513
Trevor Cox <trevoriancox@gmail.com>
2014-11-05 18:10:09 -0800

    Update for Cordova 3 compatibility.

--- src/android/CalendarPlugin.java
+++ src/android/CalendarPlugin.java
@@ -1,7 +1,7 @@
 package com.tenforwardconsulting.phonegap.plugins;
 
-import org.apache.cordova.api.CallbackContext;
-import org.apache.cordova.api.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
 import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONException;
